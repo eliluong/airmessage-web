4e5f51b243d8822aec5b5d9223eccde8f2bda428
eliluong <eliluong@gmail.com>
2025-11-17 18:44:31 -0800

    Fix contact search member type handling

--- src/components/messaging/master/Sidebar.tsx
+++ src/components/messaging/master/Sidebar.tsx
@@ -222,7 +222,9 @@ export default function Sidebar(props: {
                         }
 
                         if(conversation.members !== undefined) {
-                                return conversation.members.some((member) => member.toLowerCase().includes(lowerQuery));
+                                return conversation.members.some((memberAddress) =>
+                                        memberAddress.toLowerCase().includes(lowerQuery)
+                                );
                         }
 
                         return false;
